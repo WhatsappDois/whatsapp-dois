90d5101462b2c594e831d2d25b146b4948fef91a
menegass0 <victormenegasso@unisantos.br>
2024-05-31 14:12:04 -0300

    adcionando limitador de request para evitar ddos

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,15 @@
-import { Server } from 'socket.io';
+const { Server } = require('socket.io');
 
 const io = new Server(9000, {
     cors: {
         origin: 'http://localhost:3000',
     }, 
-})
+});
 
 let users = [];
+let rateLimitWindow = 60000; // 1 minute
+let maxRequestsPerWindow = 100;
+let requestCounts = {}; 
 
 const addUser = (userData, socketId) => {
     !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
@@ -20,25 +23,51 @@ const getUser = (userId) => {
     return users.find(user => user.sub === userId);
 }
 
-io.on('connection',  (socket) => {
-    console.log('user connected')
+// Rate limiting middleware
+const rateLimiter = (socket, next) => {
+    const socketId = socket.id;
+    if (!requestCounts[socketId]) {
+        requestCounts[socketId] = {
+            count: 0,
+            timer: setTimeout(() => {
+                delete requestCounts[socketId];
+            }, rateLimitWindow)
+        };
+    }
 
-    //connect
+    if (requestCounts[socketId].count >= maxRequestsPerWindow) {
+        return next(new Error('Rate limit exceeded'));
+    }
+
+    requestCounts[socketId].count++;
+    next();
+}
+
+io.use(rateLimiter);
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    // connect
     socket.on("addUser", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-    })
+    });
 
-    //send message
+    // send message
     socket.on('sendMessage', (data) => {
         const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
-    })
+        if (user) {
+            io.to(user.socketId).emit('getMessage', data);
+        }
+    });
 
-    //disconnect
+    // disconnect
     socket.on('disconnect', () => {
         console.log('user disconnected');
         removeUser(socket.id);
         io.emit('getUsers', users);
-    })
-})
+    });
+});
+
+console.log('Server is running on port 9000');
