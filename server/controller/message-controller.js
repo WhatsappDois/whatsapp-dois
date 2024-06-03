4108a40625fcb1d1d7db9d021bd7958c445cfc6f
menegass0 <124715996+menegass0@users.noreply.github.com>
2024-06-02 23:09:22 -0300

    Update message-controller.js

--- server/controller/message-controller.js
+++ server/controller/message-controller.js
@@ -1,22 +1,12 @@
 import Message from "../model/Message.js";
 import Conversation from '../model/Conversation.js';
-import CryptoJS from 'crypto-js';
 
-const decryptionKey = 'testeCrypto123';
-
-const decryptMessage = (encryptedMessage) => {
-    const bytes = CryptoJS.AES.decrypt(encryptedMessage, decryptionKey);
-    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-};
 
 export const newMessage = async (request, response) => {
-    const decryptedMsg = decryptMessage(request.body); 
-
-    const newMessage = new Message(decryptedMsg);
-
+    const newMessage = new Message(request.body);
     try {
         await newMessage.save();
-        await Conversation.findByIdAndUpdate(decryptedMsg.conversationId, { message: decryptedMsg.text });
+        await Conversation.findByIdAndUpdate(request.body.conversationId, { message: request.body.text });
         response.status(200).json("Message has been sent successfully");
     } catch (error) {
         response.status(500).json(error);
